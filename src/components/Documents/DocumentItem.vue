92330370135872209bfdb1a2c5e95cab743ccb52
xlmrf <spaceeternal8@gmail.com>
2023-03-21 00:15:01 +0200

    Update DocumentItem.vue

--- src/components/Documents/DocumentItem.vue
+++ src/components/Documents/DocumentItem.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="document-item">
-    <h3>
-      <router-link :to="'/document/'+document.id">
-        {{document.title}}
-      </router-link>
-    </h3>
-    <div>
+    <div class="first-piece">
+      <h3>
+        <router-link :to="'/document/'+document.id">
+          {{document.title}}
+        </router-link>
+      </h3>
+      <div>
       <span>
         {{getTypes.find(item => item.id === document.type_id)?.name}}
       </span>
-    </div>
-    <div>
-      <span v-for="author in document.authors">
-        {{author.last_name}} {{author.first_name?author.first_name[0]:''}}.
+      </div>
+      <div class="authors">
+      <span class="author-item" v-for="author in document.authors">
+        <router-link class="author-item-link" :to="'/profile/'+author.user_id" v-if="author.user_id">
+          {{author.last_name}} {{author.first_name?author.first_name[0]:''}}
+        </router-link>
+        <span v-else>
+          {{author.last_name}} {{author.first_name?author.first_name[0]:''}}.
+        </span>
       </span>
-    </div>
-    <span>
+      </div>
+      <span>
       <span v-for="keyword in document.keywords">
         {{keyword}};
       </span>
     </span>
-    <span>seen: {{document.views}}</span>
-    {{document.publication_date}}
+      <span>seen: {{document.views}}</span>
+      {{document.publication_date}}
+    </div>
+    <div class="second-piece">
+      eawegawe
+    </div>
   </div>
 </template>
 
@@ -37,17 +47,24 @@ export default {
 </script>
 
 <style scoped>
-
 .document-item{
   display: flex;
-  flex-flow: column;
   position: relative;
   /*cursor: pointer;*/
   /*margin: 0 5%;*/
   padding: 10px;
   overflow:hidden;
   outline: none;
+  justify-content: space-between;
 }
+.first-piece{
+  display: inherit;
+  flex-flow: column;
+}
+.second-piece{
+  border: 1px solid ;
+}
+
 h3{
 
 }
@@ -59,4 +76,31 @@ h3{
   /*color: #0F0F0F;*/
   color: #0969DA;
 }
+.author-item-link{
+  color: #0969DA;
+}
+.author-item-link:hover{
+  text-decoration: underline #3684DD;
+}
+.author-item{
+  position: relative;
+  padding: 10px;
+}
+.author-item:after{
+  content: "";
+  position: absolute;
+  background: #9A9A9A;
+  width: 1px;
+  top: 25%;
+  right: 0;
+  height: 50%;
+}
+.author-item:last-child:after{
+  content: "";
+  background: transparent;
+}
+.authors{
+  margin: 10px 0;
+}
+
 </style>
